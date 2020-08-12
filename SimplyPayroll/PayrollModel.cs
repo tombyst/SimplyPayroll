dacc32be275d5d717ad59479fcd2118c9e76b27a
Tomasz Bystrek <tombys@live.com>
2020-08-12 17:07:35 +0100

    add xml comments

--- SimplyPayroll/PayrollModel.cs
+++ SimplyPayroll/PayrollModel.cs
@@ -5,12 +5,13 @@ using System.Threading.Tasks;
 
 namespace SimplyPayroll
 {
-    /// <summary>
-    /// 
-    /// </summary>
+    /// <summary> provides a context for user’s input values binding.</summary>
+    /// <remarks></remarks>
     public class PayrollModel
     {
+        /// <summary>Represents a number of hours worked from user’s input.</summary>
         public double hoursWorked;
+        /// <summary>Represents hourly rate from user’s input.</summary>
         public double hourlyRate;
     }
 }
