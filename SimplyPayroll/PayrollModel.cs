0daa14d59dd51febb9227ebc6c24e8a7c6579eae
Tomasz Bystrek <tombys@live.com>
2020-09-04 20:27:42 +0100

    add validation for hours and rate input values

--- SimplyPayroll/PayrollModel.cs
+++ SimplyPayroll/PayrollModel.cs
@@ -1,16 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace SimplyPayroll
 {
+
     /// <summary> Provides a context for user’s input values binding.</summary>
     public class PayrollModel
     {
         /// <summary>Represents a number of hours worked from user’s input.</summary>
-        public double hoursWorked;
+        [Range(0.25, 48, ErrorMessage = "Hours worked must be between 0.25 and 48.")]
+        public double HoursWorked { get; set; }
+
         /// <summary>Represents hourly rate from user’s input.</summary>
-        public double hourlyRate;
+        [Required]
+        [Range(10.10, 100, ErrorMessage = "Hourly rate must be between 10 and 100.")]
+        [Display(Name = "Hourly rate")]
+        public double HourlyRate { get; set; }
+
     }
 }
