945ecb6c687174b3bd52d193845b28231c9be3d8
Tomasz Bystrek <tombys@live.com>
2020-11-11 21:05:48 +0000

    change the range of valid numbers (3-48 instead of 0.25-48)

--- SimplyPayroll/PayrollModel.cs
+++ SimplyPayroll/PayrollModel.cs
@@ -8,13 +8,15 @@ namespace SimplyPayroll
     public class PayrollModel
     {
         /// <summary>Represents a number of hours worked from user’s input.</summary>
-        [Range(0.25, 48, ErrorMessage = "Hours worked must be between 0.25 and 48.")]
+        [Required]
+        [Range(3, 48, ErrorMessage = "Hours worked must be between 3 and 48.")]
+        [Display(Name = "Hours Worked")]
         public double HoursWorked { get; set; }
 
         /// <summary>Represents hourly rate from user’s input.</summary>
         [Required]
         [Range(10.10, 100, ErrorMessage = "Hourly rate must be between 10 and 100.")]
-        [Display(Name = "Hourly rate")]
+        [Display(Name = "Hourly rate")]        
         public double HourlyRate { get; set; }
 
     }
